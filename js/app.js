7c4bf55d9fbc88a40ebefc811b70fe8036665023
MasudMahamud <mahamud.al.msdud@gmail.com>
2021-11-27 02:14:02 +0600

    book-list

---
+++ js/app.js
@@ -0,0 +1,32 @@
+
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const year = document.querySelector('#year');
+const addBtn = document.querySelector('#btn');
+const bookList = document.querySelector('#book-list');
+
+
+btn.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    if (title.value == '' && author.value == '' && year.value == '') {
+        alert('No title or author')
+    }
+    else{
+        const newRow = document.createElement('tr');
+
+        const newTitle = document.createElement('td');
+        newTitle.innerHTML = title.value;
+        newRow.appendChild(newTitle);
+
+        const newAuthor = document.createElement('td');
+        newAuthor.innerHTML = author.value;
+        newRow.appendChild(newAuthor);
+
+        const newYear = document.createElement('td');
+        newYear.innerHTML = year.value;
+        newRow.appendChild(newYear);
+
+        bookList.appendChild(newRow);
+    }
+})
